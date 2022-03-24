5d1076286edeffd113c6d73cf7fec6b2381b6c3b
niltonc <niltoncesarjr1@gmail.com>
2022-03-24 13:14:50 -0300

    new update: "Sing In layout update"

--- src/screens/auth/signin/index.tsx
+++ src/screens/auth/signin/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-export default function SignIn() {
+import { RootStackScreenProps } from '../../../types';
+
+import * as S from '../../../styles';
+
+export default function SingUp({ navigation }: RootStackScreenProps<'SignIn'>) {
   return (
-    <View>
-      <Text>Login PAGE</Text>
-    </View>
+    <S.Container>
+      <S.TitleText>Olá!</S.TitleText>
+      <S.TextInputContainer>
+        <S.TextInput placeholder="Email" placeholderTextColor="#B1BEC2" />
+        <S.TextInput placeholder="Semha" placeholderTextColor="#B1BEC2" />
+
+        <S.ButtonPrimary>
+          <S.ButtonText>Entrar</S.ButtonText>
+        </S.ButtonPrimary>
+      </S.TextInputContainer>
+
+      <S.SimpleTextContainer>
+        <S.SimpleTextPrimary>Não tem uma conta?</S.SimpleTextPrimary>
+        <S.SimpleTextSecundary>Criar conta</S.SimpleTextSecundary>
+      </S.SimpleTextContainer>
+    </S.Container>
   );
 }
