72e1851daf50ee76a9d8bfafbc7f7d5eac13430f
niltonc <niltoncesarjr1@gmail.com>
2022-03-25 13:26:18 -0300

    feat(firebase): auth structure

---
+++ src/services/firebase/auth.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  verifyPasswordResetCode,
+} from 'firebase/auth';
+
+import app from './firebase';
+
+const auth = getAuth(app);
+
+export async function signin(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function signup(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function signout() {
+  return signOut(auth);
+}
+
+export function verifypasswordresetcode(email) {
+  return verifyPasswordResetCode(auth, email);
+}
