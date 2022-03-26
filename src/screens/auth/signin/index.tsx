039cfaee846597311bacb8a775e4afabb2c98d79
niltonc <niltoncesarjr1@gmail.com>
2022-03-26 19:35:17 -0300

    feat(signin/signup): add yup validation and moti animation

--- src/screens/auth/signin/index.tsx
+++ src/screens/auth/signin/index.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import { Text } from 'react-native';
+
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 import emailicon from '../../../assets/icons/email.png';
 import eyeicon from '../../../assets/icons/eye.png';
 import lockicon from '../../../assets/icons/lock.png';
 import * as Button from '../../../components/Button';
-import * as Text from '../../../components/Input';
+import * as Texts from '../../../components/Input';
 import { useDataStore } from '../../../context/store';
 import { signin } from '../../../services/firebase/auth';
 import { RootStackScreenProps } from '../../../types';
@@ -12,59 +16,112 @@ import { RootStackScreenProps } from '../../../types';
 import * as S from '../../../styles';
 
 export default function SingUp({ navigation }: RootStackScreenProps<'SignIn'>) {
+  const [errorFirebase, setErrorFirebase] = useState('');
+  const [securityPass, setSecurityPass] = useState(true);
+
   const { setInicialRouterName } = useDataStore();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignIn = async () => {
-    await signin(email, password);
-    setInicialRouterName('HomePage');
-    navigation.navigate('HomePage');
+  const handleSignIn = async (values) => {
+    const { email, password } = values;
+    const user = await signin(email, password).catch((Error) => {
+      if (Error.code === 'auth/user-not-found') {
+        setErrorFirebase('O email inserido não corresponde a nenhuma conta');
+      }
+      if (Error.code === 'auth/wrong-password') {
+        setErrorFirebase('A senha inserida está incorreta');
+      }
+      if (Error.code === 'auth/too-many-requests') {
+        setErrorFirebase(
+          'Você errou a senha varias vezes. Tente novamente mais tarde'
+        );
+      }
+    });
+    if (user) {
+      setInicialRouterName('HomePage');
+      navigation.navigate('HomePage');
+    }
   };
 
+  const validationSignIn = yup.object().shape({
+    email: yup
+      .string()
+      .label('Email')
+      .email('Email Inválido.')
+      .required('Digite seu Email')
+      .trim()
+      .strict(),
+    password: yup
+      .string()
+      .label('Senha')
+      .required('Digite sua Senha')
+      .min(4, ({ min }) => `A senha precisa ser maior que ${min} caracteres`),
+  });
+
   return (
-    <S.Container>
-      <S.ContainerTitleBold>
-        <S.TitleBold>Olá!</S.TitleBold>
-        <S.TitleBoldSec>Seja bem-vindo.</S.TitleBoldSec>
-      </S.ContainerTitleBold>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={validationSignIn}
+      onSubmit={handleSignIn}
+    >
+      {({
+        errors,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isValid,
+        touched,
+        values,
+      }) => (
+        <S.Container>
+          <S.ContainerTitleBold>
+            <S.TitleBold>Olá!</S.TitleBold>
+            <S.TitleBoldSec>Seja bem-vindo.</S.TitleBoldSec>
+          </S.ContainerTitleBold>
 
-      <S.ContainerTextInput>
-        <S.ContainerInput>
-          <Text.Icon source={emailicon} />
-          <Text.Input
-            value={email}
-            onChangeText={(value) => setEmail(value)}
-            placeholder="Email"
-            placeholderTextColor="#B1BEC2"
-          />
-        </S.ContainerInput>
+          <S.ContainerTextInput>
+            <Text>{errorFirebase}</Text>
+            <S.ContainerInput>
+              <Texts.Icon source={emailicon} />
+              <Texts.Input
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                placeholder="Email"
+                placeholderTextColor="#B1BEC2"
+              />
+            </S.ContainerInput>
+            {errors.email && touched.email && <Text>{errors.email}</Text>}
 
-        <S.ContainerInput>
-          <Text.IconLock source={lockicon} />
-          <Text.Input
-            value={password}
-            onChangeText={(value) => setPassword(value)}
-            placeholder="Senha"
-            placeholderTextColor="#B1BEC2"
-            secureTextEntry
-          />
+            <S.ContainerInput>
+              <Texts.IconLock source={lockicon} />
+              <Texts.Input
+                value={values.password}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                placeholder="Senha"
+                placeholderTextColor="#B1BEC2"
+                secureTextEntry={securityPass}
+              />
 
-          <Text.Iconeye source={eyeicon} />
-        </S.ContainerInput>
+              <Texts.Iconeye source={eyeicon} />
+            </S.ContainerInput>
+            {errors.password && touched.password && (
+              <Text>{errors.password}</Text>
+            )}
 
-        <Button.Margin onPress={handleSignIn}>
-          <Button.Text>Entrar</Button.Text>
-        </Button.Margin>
-      </S.ContainerTextInput>
+            <Button.Margin disabled={!isValid} onPress={handleSubmit}>
+              <Button.Text>Entrar</Button.Text>
+            </Button.Margin>
+          </S.ContainerTextInput>
 
-      <S.ContainerText>
-        <S.TextSimple>Não tem uma conta?</S.TextSimple>
-        <S.TextClick onPress={() => navigation.navigate('SignUp')}>
-          <S.TextPrimary>Criar conta</S.TextPrimary>
-        </S.TextClick>
-      </S.ContainerText>
-    </S.Container>
+          <S.ContainerText>
+            <S.TextSimple>Não tem uma conta?</S.TextSimple>
+            <S.TextClick onPress={() => navigation.navigate('SignUp')}>
+              <S.TextPrimary>Criar conta</S.TextPrimary>
+            </S.TextClick>
+          </S.ContainerText>
+        </S.Container>
+      )}
+    </Formik>
   );
 }
--- src/screens/auth/signup/index.tsx
+++ src/screens/auth/signup/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { Text } from 'react-native';
+
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 import emailicon from '../../../assets/icons/email.png';
 import eyeicon from '../../../assets/icons/eye.png';
 import lockicon from '../../../assets/icons/lock.png';
 import usericon from '../../../assets/icons/user.png';
 import * as Button from '../../../components/Button';
-import * as Text from '../../../components/Input';
+import * as Texts from '../../../components/Input';
 import { useDataStore } from '../../../context/store';
 import { signup } from '../../../services/firebase/auth';
 import { RootStackScreenProps } from '../../../types';
@@ -14,75 +18,148 @@ import * as S from '../../../styles';
 
 export default function SingUp({ navigation }: RootStackScreenProps<'SignUp'>) {
   const { setName, setInicialRouterName } = useDataStore();
+  const [errorFirebase, setErrorFirebase] = useState('');
+  const [securityPass, setSecurityPass] = useState(true);
 
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignUp = async () => {
-    await signup(email, password);
-    setName(userName);
-    setInicialRouterName('HomePage');
-    navigation.navigate('HomePage');
+  const handleSignUp = async (values) => {
+    const { name, email, password } = values;
+    const user = await signup(email, password).catch((Error) => {
+      console.log('Error: ', Error);
+      if (Error.code === 'auth/email-already-exists') {
+        setErrorFirebase('O email inserido já está em uso');
+      }
+      if (Error.code === 'auth/email-already-in-use') {
+        setErrorFirebase('O email inserido já está em uso');
+      }
+    });
+    if (user) {
+      setName(name);
+      setInicialRouterName('SignIn');
+      navigation.navigate('SignIn');
+    }
   };
 
+  const validationSignUp = yup.object().shape({
+    name: yup
+      .string()
+      .label('Nome')
+      .required('Digite seu Nome')
+      .min(4, ({ min }) => `O nome precisa ser maior que  ${min} caracteres`),
+    email: yup
+      .string()
+      .label('Email')
+      .email('Email Inválido.')
+      .required('Digite seu Email')
+      .trim()
+      .strict(),
+    password: yup
+      .string()
+      .label('Senha')
+      .required('Digite sua Senha')
+      .min(6, ({ min }) => `A senha precisa ser maior que ${min} caracteres`)
+      .max(30, ({ max }) => `A senha precisa ser menor que ${max} caracteres`),
+    passwordConfirmation: yup
+      .string()
+      .required('Você precisar confirmar sua senha')
+      .oneOf([yup.ref('password'), null], 'As senhas precisam ser iguais'),
+  });
+
   return (
-    <S.Container>
-      <S.ContainerTitleSemiBold>
-        <S.TitleSemiBold>Criar Conta</S.TitleSemiBold>
-      </S.ContainerTitleSemiBold>
-      <S.ContainerTextInputSec>
-        <S.ContainerInput>
-          <Text.Icon source={usericon} />
-          <Text.Input
-            value={userName}
-            onChangeText={(value) => setUserName(value)}
-            placeholder="Nome completo"
-            placeholderTextColor="#B1BEC2"
-          />
-        </S.ContainerInput>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirmation: '',
+      }}
+      validationSchema={validationSignUp}
+      onSubmit={handleSignUp}
+    >
+      {({
+        errors,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isValid,
+        touched,
+        values,
+      }) => (
+        <S.Container>
+          <S.ContainerTitleSemiBold>
+            <S.TitleSemiBold>Criar Conta</S.TitleSemiBold>
+          </S.ContainerTitleSemiBold>
 
-        <S.ContainerInput>
-          <Text.Icon source={emailicon} />
-          <Text.Input
-            value={email}
-            onChangeText={(value) => setEmail(value)}
-            placeholder="Email"
-            placeholderTextColor="#B1BEC2"
-          />
-        </S.ContainerInput>
-        <S.ContainerInput>
-          <Text.IconLock source={lockicon} />
-          <Text.Input
-            value={password}
-            onChangeText={(value) => setPassword(value)}
-            placeholder="Senha"
-            placeholderTextColor="#B1BEC2"
-            secureTextEntry
-          />
-          <Text.Iconeye source={eyeicon} />
-        </S.ContainerInput>
-        <S.ContainerInput>
-          <Text.IconLock source={lockicon} />
-          <Text.Input
-            placeholder="Confirmar Senha"
-            placeholderTextColor="#B1BEC2"
-            secureTextEntry
-          />
-          <Text.Iconeye source={eyeicon} />
-        </S.ContainerInput>
+          <S.ContainerTextInputSec>
+            <Text>{errorFirebase}</Text>
+            <S.ContainerInput>
+              <Texts.Icon source={usericon} />
+              <Texts.Input
+                value={values.name}
+                onChangeText={handleChange('name')}
+                onBlur={handleBlur('name')}
+                placeholder="Nome completo"
+                placeholderTextColor="#B1BEC2"
+              />
+            </S.ContainerInput>
+            {errors.name && touched.name && <Text>{errors.name}</Text>}
 
-        <Button.Margin onPress={handleSignUp}>
-          <Button.Text>Cadastrar</Button.Text>
-        </Button.Margin>
-      </S.ContainerTextInputSec>
+            <S.ContainerInput>
+              <Texts.Icon source={emailicon} />
+              <Texts.Input
+                value={values.email}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                placeholder="Email"
+                placeholderTextColor="#B1BEC2"
+              />
+            </S.ContainerInput>
+            {errors.email && touched.email && <Text>{errors.email}</Text>}
 
-      <S.ContainerText>
-        <S.TextSimple>Já tem uma conta?</S.TextSimple>
-        <S.TextClick onPress={() => navigation.navigate('SignIn')}>
-          <S.TextPrimary>Faça o login</S.TextPrimary>
-        </S.TextClick>
-      </S.ContainerText>
-    </S.Container>
+            <S.ContainerInput>
+              <Texts.IconLock source={lockicon} />
+              <Texts.Input
+                value={values.password}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                placeholder="Senha"
+                placeholderTextColor="#B1BEC2"
+                secureTextEntry={securityPass}
+              />
+              <Texts.Iconeye source={eyeicon} />
+            </S.ContainerInput>
+            {errors.password && touched.password && (
+              <Text>{errors.password}</Text>
+            )}
+
+            <S.ContainerInput>
+              <Texts.IconLock source={lockicon} />
+              <Texts.Input
+                value={values.passwordConfirmation}
+                onChangeText={handleChange('passwordConfirmation')}
+                onBlur={handleBlur('passwordConfirmation')}
+                placeholder="Confirmar Senha"
+                placeholderTextColor="#B1BEC2"
+                secureTextEntry={securityPass}
+              />
+              <Texts.Iconeye source={eyeicon} />
+            </S.ContainerInput>
+            {errors.passwordConfirmation && touched.passwordConfirmation && (
+              <Text>{errors.passwordConfirmation}</Text>
+            )}
+
+            <Button.Margin disabled={!isValid} onPress={handleSubmit}>
+              <Button.Text>Cadastrar</Button.Text>
+            </Button.Margin>
+          </S.ContainerTextInputSec>
+
+          <S.ContainerText>
+            <S.TextSimple>Já tem uma conta?</S.TextSimple>
+            <S.TextClick onPress={() => navigation.navigate('SignIn')}>
+              <S.TextPrimary>Faça o login</S.TextPrimary>
+            </S.TextClick>
+          </S.ContainerText>
+        </S.Container>
+      )}
+    </Formik>
   );
 }
