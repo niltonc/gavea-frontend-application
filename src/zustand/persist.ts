8c391367224b6f04a5c32a5489f2400ce82d6fd1
niltonc <niltoncesarjr1@gmail.com>
2022-03-26 15:46:53 -0300

    refactor(zustand): update AsyncStorage libary

--- src/zustand/persist.ts
+++ src/zustand/persist.ts
@@ -1,5 +1,4 @@
-import { AsyncStorage } from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { configurePersist } from 'zustand-persist';
 
 const { persist, purge } = configurePersist({
