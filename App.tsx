ce0280ec6112c10c654a4a64dd8cb72d38b4256e
niltonc <niltoncesarjr1@gmail.com>
2022-03-27 17:10:23 -0300

    refactor(app): remove code comment

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { StatusBar } from 'expo-status-bar';
-// import { PersistGate } from 'zustand-persist';
 
 import Routes from './src/routes';
 
@@ -12,10 +11,8 @@ import GaveaTheme from './src/styles/theme/theme';
 export default function App() {
   return (
     <ThemeProvider theme={GaveaTheme}>
-      {/* <PersistGate> */}
       <Routes />
       <StatusBar />
-      {/* </PersistGate> */}
     </ThemeProvider>
   );
 }
